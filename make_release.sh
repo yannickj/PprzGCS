59513090687003c23babb90fef284b885d847ff1
Fabien-B <fabien.bonneval@gmail.com>
2021-02-18 02:50:53 +0100

    Make deb more compliant.

--- make_release.sh
+++ make_release.sh
@@ -15,12 +15,14 @@ mkdir -p $lib
 
 cp -r $GITHUB_WORKSPACE/data/* $share
 cp  $PprzGCS $bin/pprzgcs
+strip --strip-unneeded $bin/pprzgcs
 
 path_pprzlink=$(ldd $PprzGCS | cut -d " " -f 3 | grep libpprzlink)
 path_tinyxml2=$(ldd $PprzGCS | cut -d " " -f 3 | grep tinyxml2)
 
 cp $path_pprzlink $lib
 cp $path_tinyxml2 $lib
+strip --strip-unneeded $lib/*
 
 patchelf --set-rpath '$ORIGIN/../lib/pprzgcs/' $bin/pprzgcs
 
