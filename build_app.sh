0aa17ad3692aeb487b6cd6cf10422afae03a0b49
Fabien-B <fabien.bonneval@gmail.com>
2021-02-18 02:09:12 +0100

    build configuration.

--- build_app.sh
+++ build_app.sh
@@ -2,7 +2,7 @@
 
 local_install=$GITHUB_WORKSPACE/build/local_install
 
-cmake -S . -B build -DCMAKE_PREFIX_PATH="$local_install"
+cmake -S . -B build -DCMAKE_PREFIX_PATH="$local_install" -DBUILD_FOR_DEB=ON -DPROJ_ON_SYSTEM=ON
 cmake --build build -j$(nproc)
 echo "Build directory is $(pwd)/build"
 ls -l $(pwd)/build
